e6457140a554de5010390bc5c2cc9b277970450e
Fate2664 <18pielou@gmail.com>
2026-03-31 23:06:34 +0200

    Added tutorial screen

---
+++ 3DPlatformer/Assets/References.cs
@@ -0,0 +1,5 @@
+/*
+ * Edenity, 2023. Idyllic Fantasy Nature. ArtStation.com <https://assetstore.unity.com/publishers/67496> [Accessed 31 March 2026].
+ * Iglesias, K.I, 2025. Human Basic Motions FREE. Keviniglesias.com <https://assetstore.unity.com/publishers/36307> [Accessed 31 March 2026].
+ * Polytope Studio, 2026. Low Poly Environment - Nature Free - LOW POLY MEDIEVAL FANATASY SERIES. ArtStation.com <https://assetstore.unity.com/publishers/35251> [Accessed 31 March 2026].
+ */
