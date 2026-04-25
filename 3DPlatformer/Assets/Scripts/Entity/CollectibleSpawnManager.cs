3d80f37e24d8bce4a488ac4521ca2832a239d17c
Fate2664 <18pielou@gmail.com>
2026-04-25 16:56:23 +0200

    Finished entity spawner

---
+++ 3DPlatformer/Assets/Scripts/Entity/CollectibleSpawnManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Platformer
+{
+    public class CollectibleSpawnManager : EntitySpawneManager
+    {
+        [SerializeField] private CollectibleData[] collectibleData;
+        [SerializeField] private float spawnRate = 1f;
+
+        private EntitySpawner<Collectible> spawner;
+
+        private CountDownTimer spawnTimer;
+        private int counter;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            spawner = new EntitySpawner<Collectible>(new EntityFactory<Collectible>(collectibleData),
+                spawnPointStrategy);
+
+            spawnTimer = new CountDownTimer(spawnRate);
+            spawnTimer.OnTimerStop += () =>
+            {
+                if (counter++ >= spawnPoints.Length)
+                {
+                    spawnTimer.Stop();
+                    return;
+                }
+                Spawn();
+                spawnTimer.Start();
+            };
+        }
+        
+        void Start() => spawnTimer.Start();
+        void Update() => spawnTimer.Tick(Time.deltaTime);
+
+        public override void Spawn() => spawner.Spawn();
+    }
+}
