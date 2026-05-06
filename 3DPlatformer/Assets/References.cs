156aa1577683a93d164b33a1924f0931fb73cab1
Fate2664 <18pielou@gmail.com>
2026-05-06 21:19:40 +0200

    Did references

--- 3DPlatformer/Assets/References.cs
+++ 3DPlatformer/Assets/References.cs
@@ -2,4 +2,8 @@
  * Edenity, 2023. Idyllic Fantasy Nature. ArtStation.com <https://assetstore.unity.com/publishers/67496> [Accessed 31 March 2026].
  * Iglesias, K.I, 2025. Human Basic Motions FREE. Keviniglesias.com <https://assetstore.unity.com/publishers/36307> [Accessed 31 March 2026].
  * Polytope Studio, 2026. Low Poly Environment - Nature Free - LOW POLY MEDIEVAL FANATASY SERIES. ArtStation.com <https://assetstore.unity.com/publishers/35251> [Accessed 31 March 2026].
+ * 3D platformer in Unity - PlayerController (Part 1), 2023. git-amend. [YouTube video] Available at: <https://www.youtube.com/watch?v=--_CH5DYz0M&list=PLnJJ5frTPwRNdyRAD4oBtG1eUVBuj2h1O> [Accessed 6 May 2026].
+ * How to create a LinkedList from scratch using C# and Visual Studio, 2021. Gen Grievous. [YouTube video] Available at: <https://www.youtube.com/watch?v=8TGFk_zUS9A> [Accessed 6 May 2026].
+ * 1st AND 3RD PERSON CONTROLLER - COMPLETE COURSE (Part 2), 2024. spaderdabomb. [YouTube video] Available at: <https://www.youtube.com/watch?v=PIFQbxMgT0c> [Accessed 6 May 2026].
+ * Stylized Core by Z, 2026. Stylized Player Character (Free). Stylized Core <https://stylizedcore.artstation.com/> [Accessed 6 May 2026].
  */
